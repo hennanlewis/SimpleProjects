f54bc0f4bcfc099b2b63dac533bf9cddc36519e8
Hennan Lewis <hennan.lewis@gmail.com>
2021-08-23 04:32:48 -0300

    v0.2

--- twitch-speaker/app.js
+++ twitch-speaker/app.js
@@ -1,34 +1,62 @@
-let channelName = ''
-
 const form = document.querySelector('#form')
 form.addEventListener('submit', (event) => {
 	event.preventDefault()
+	let channelName = document.querySelector('#input').value
+	console.log('Conectanto a canal...')
+	connectToTwitchChat(channelName)
+})
 
-
-	channelName = document.querySelector('#input').value
-
-	const client = new tmi.Client({
-		channels: [channelName]
-	})
-
+const connectToTwitchChat = (channelName) => {
+	const client = new tmi.Client({ channels: [channelName] })
 	client.connect()
+	.then(() => console.log(`Conectado a ${channelName}`))
+	.catch(error => console.log('Não foi possível conectar'))
 
 	var synth = window.speechSynthesis
 	var messageBox = document.querySelector('#messageBox')
 	var html = ''
 
 	client.on('message', (channel, tags, message, self) => {
-		// "Alca: Hello, World!"
 		if (self) return
-		if (tags.username !== 'nightbot') {
-			var utterThis = new SpeechSynthesisUtterance(`${tags.username} falou.`)
-			utterThis.lang = 'pt'
-			synth.speak(utterThis)
-			html += `<div ><span class="user">${tags.username}:</span>
-			<span class="message">${message}</span></div>`
-			messageBox.innerHTML = html
-			messageBox.scrollTop = messageBox.scrollHeight
-		}
-	})
 
-})
+		html += `<div><span class="user" style="color: ${tags.color}">${tags.username}</span>:
+		<span class="message">${message}</span></div>`
+		messageBox.innerHTML = html
+
+		if(messageBox.scrollHeight - messageBox.scrollTop < 400)
+			messageBox.scrollTop = messageBox.scrollHeight
+
+		speakMessage(tags.username, message)
+	})
+}
+
+const speakMessage = (username, message) => {
+	let mode = document.querySelector('select[name="speaker"]').value
+	let speed = Array.from(document.querySelectorAll('input[name="speed"]'))
+		.filter(item => item.checked)[0].value
+
+	let synth = window.speechSynthesis
+	let utterThis = new SpeechSynthesisUtterance('Mensagem de voz')
+	if(mode === 'nothing') return
+	if(mode === 'username') {
+		utterThis = new SpeechSynthesisUtterance(`${username} mandou uma mensagem.`)
+		utterThis.lang = 'pt'
+		utterThis.rate = parseInt(speed)
+		synth.speak(utterThis)
+		return
+	}
+	if(mode === 'message') {
+		utterThis = new SpeechSynthesisUtterance(`${message}.`)
+		utterThis.lang = 'pt'
+		utterThis.rate = parseInt(speed)
+		synth.speak(utterThis)
+		return
+	}
+	if(mode === 'all') {
+		utterThis = new SpeechSynthesisUtterance(`${username} disse ${message}.`)
+		utterThis.lang = 'pt'
+		utterThis.rate = parseInt(speed)
+		synth.speak(utterThis)
+		return
+	}
+}
